248d12fbdc0771febf13dcfc1a92bf94d0027a68
Srini <s@srin.me>
2020-01-24 04:20:20 -0500

    brand changed

--- cmd/trace.go
+++ cmd/trace.go
@@ -1,11 +1,11 @@
 package cmd
 // Go package generator
 // File: cmd.h
-const buildTime = "Thu Jan 23 2020 04:45:07"
+const buildTime = "Fri Jan 24 2020 04:20:20"
 const versionMajor = 0
-const versionMinor = 1
+const versionMinor = 2
 const versionBuild = 1
 const repoURL = "git@gitlab.com:privatetutor/projectlets/go.git"
 const branchName = "master"
-const shortCommitId = "589c14b"
-const longCommitId = "589c14bf6f4792177d9a9b0b09f727ac0349c51d"
+const shortCommitId = "f03ca61"
+const longCommitId = "f03ca61233e1e6776d0dbefbdf3c36d1ddf61e9e"
