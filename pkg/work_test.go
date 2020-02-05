ac20a2bcc40b896497b40cf696504bc89f525e85
Srini <s@srin.me>
2020-02-05 13:16:08 -0500

    create test home tmp dir

--- pkg/work_test.go
+++ pkg/work_test.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	"log"
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -15,6 +16,10 @@ func TestCreateWorkArea(t *testing.T) {
 	home, err := homedir.Dir()
 	if err == nil {
 		hometemp := filepath.Join(home, "tmp")
+		fi,err := os.Stat(hometemp)
+		if os.IsNotExist(err) [
+			os.Mkdir(hometemp)
+		]
 		CreateWorkArea(hometemp)
 	} else {
 		log.Printf("%s", err)
